4c7914c0078b17d305feed9de6f059aea45b22dc
anikaa137 <anikaa137@gmail.com>
2021-05-27 00:28:54 +0600

    again solved

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,25 +7,25 @@ import img from '../../img/logo.png'
 
 
 const Navbar = () => {
-    
-   
+
+
     return (
-        
+
 <nav class='navbar navbar-expand-lg navbar-light sticky '>
   <div class="container">
     <a class="navbar-brand" href="#">
         <span className='mr-4'>
           <img src={img} alt="" />
         </span>
-        
+
     </a>
-  
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-  
+
     <div class="collapse navbar-collapse" id="navbarNav">
-      
+
       <ul class="navbar-nav navbar-customize navItem">
          <li>
              <Link to='/home'><span><FontAwesomeIcon icon={faSearch} /></span>Search</Link>
@@ -33,18 +33,18 @@ const Navbar = () => {
          <li>
              <Link to='/about'>Help</Link>
          </li>
-         <li> 
-             <Link to='/dashboard'>Account</Link>  
+         <li>
+             <Link to='/dashboard'>Account</Link>
          </li>
          <li>
              <Link to='/contact'><span><FontAwesomeIcon icon={faShoppingBag} /></span></Link>
          </li>
-         
+
       </ul>
     </div>
   </div>
 </nav>
-       
+
     );
 };
 
