e7b4e5e300534d5edb31f1ebc55baf111a332531
anikaa137 <anikaa137@gmail.com>
2021-05-27 00:25:40 +0600

    solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./component/Home/Home";
 
 const App = () => {
